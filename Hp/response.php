40a2e725a0a27ae05a3d2649a615a2a22db39de3
sk <sunkejl@live.com>
2017-04-05 18:50:39 +0800

    add cookie

--- Hp/response.php
+++ Hp/response.php
@@ -12,6 +12,12 @@ header("Date: 2017");
 #header("Content-Length: 100");
 header("X-Powered-By: JS");
 header("C:2");
+var_dump(setcookie("u_a_A", "2444", time()+3600000000, "/", "artisan.com",false));
+setcookie("u_b", "23");
+#header("Set-Cookie: name=user123");
+header("Set-Cookie:u_a_A=2444; expires=Sat, 05-May-2131 02:32:44 GMT; Max-Age=3600000000; path=/; domain=artisan.com");
+header("Set-Cookie:u_b=23");
+
 #echo json_encode($_REQUEST);exit;
 var_dump($_REQUEST);
 var_dump($_SERVER);
