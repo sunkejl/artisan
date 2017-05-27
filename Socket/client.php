ff92f4586f73bd7c779fd66db6e34b3d87b77465
sk <sunkejl@live.com>
2017-05-27 19:03:05 +0800

    add socket

--- Socket/client.php
+++ Socket/client.php
@@ -12,10 +12,13 @@ $ip = "127.0.0.1";
 $port = "10000";
 $socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
 socket_connect($socket, $ip, $port);
-while ($file=file_get_contents("php://stdin", "r")){
+while ($file = file_get_contents("php://stdin", "r")) {
     var_dump($file);
+    //socket_send($socket,12,2,MSG_EOF);
     socket_write($socket, 123, 3);
+    var_dump(socket_strerror(socket_last_error()));
     var_dump(socket_read($socket, 2048));
+    var_dump(socket_strerror(socket_last_error()));
 //    while($out = socket_read($socket, 2048)) {
 //        echo "接收服务器回传信息成功！\n";
 //        echo "接收的内容为:",$out;
