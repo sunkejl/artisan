891dab64e21d6376809b3a581bfa203512d72f67
sk <sunkejl@live.com>
2017-05-31 13:51:43 +0800

    -m s.php

--- Socket/client.php
+++ Socket/client.php
@@ -9,18 +9,17 @@
 
 
 $ip = "127.0.0.1";
-$port = "10000";
+$port = "8879";
 $socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
 socket_connect($socket, $ip, $port);
+if (false != ($out = socket_read($socket, 255))) {
+    echo "acp: $out\n";
+}
 while ($file = file_get_contents("php://stdin", "r")) {
-    var_dump($file);
-    //socket_send($socket,12,2,MSG_EOF);
-    socket_write($socket, 123, 3);
-    var_dump(socket_strerror(socket_last_error()));
-    var_dump(socket_read($socket, 2048));
-    var_dump(socket_strerror(socket_last_error()));
-//    while($out = socket_read($socket, 2048)) {
-//        echo "接收服务器回传信息成功！\n";
-//        echo "接收的内容为:",$out;
-//    }
-};
+    echo $file;
+    socket_write($socket, $file, strlen($file));
+    if (false != ($out = socket_read($socket, 255))) {
+        echo "acp: $out\n";
+    }
+}
+socket_close($socket);
--- Socket/server.php
+++ Socket/server.php
@@ -6,16 +6,20 @@
  * Time: 15:08
  */
 $ip = "127.0.0.1";
-$port = "88889";
+$port = "8879";
 $socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
 socket_bind($socket, $ip, $port);
 socket_listen($socket, 5);
 do {
     $client = socket_accept($socket);
-    socket_write($client, "sss", 3);
+    socket_write($client, "welcome", 256);
     do {
-        $buffer = socket_read($client, 2048, PHP_NORMAL_READ);
-        socket_write($client, "ddd", 3);
+        if(false==($buffer = socket_read($client, 255, PHP_NORMAL_READ)))
+        {
+            break;
+        }
+        socket_write($client, $buffer, 255);
+        echo "receive:$buffer\n";
     } while (true);
     socket_close($client);
 } while (true);
